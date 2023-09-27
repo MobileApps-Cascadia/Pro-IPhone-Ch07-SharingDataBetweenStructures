828f4278bafc5d4e4e5fca37be13742072d121fa
Mike Panitz <michpa@hotmail.com>
2023-09-26 21:40:48 -0700

    Initial Commit

---
+++ ProIPhone_Ch07_SharingDataBetweenStructures/ProIPhone_Ch07_SharingDataBetweenStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProIPhone_Ch07_SharingDataBetweenStructuresApp.swift
+//  ProIPhone_Ch07_SharingDataBetweenStructures
+//
+//  Created by Mike Panitz on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ProIPhone_Ch07_SharingDataBetweenStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
