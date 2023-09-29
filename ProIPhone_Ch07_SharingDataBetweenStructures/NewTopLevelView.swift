707082b3a9afb554edddcede041fc5ec73a2e827
Freddy <fredericdouamba2014@gmail.com>
2023-09-28 18:40:09 -0700

    Sharing Data Between Structure.

---
+++ ProIPhone_Ch07_SharingDataBetweenStructures/NewTopLevelView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTopLevelView.swift
+//  ProIPhone_Ch07_SharingDataBetweenStructures
+//
+//  Created by Student Account on 9/28/23.
+//
+
+import SwiftUI
+
+struct NewTopLevelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTopLevelView()
+}
---
+++ ProIPhone_Ch07_SharingDataBetweenStructures/StudentCourseModels.swift
@@ -0,0 +1,8 @@
+//
+//  StudentCourseModels.swift
+//  ProIPhone_Ch07_SharingDataBetweenStructures
+//
+//  Created by Student Account on 9/28/23.
+//
+
+import Foundation
