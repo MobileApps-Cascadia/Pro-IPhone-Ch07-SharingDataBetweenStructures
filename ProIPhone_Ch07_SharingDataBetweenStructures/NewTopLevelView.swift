d6377980418288935f0c9341d123c21def913299
Freddy <fredericdouamba2014@gmail.com>
2023-09-28 18:49:52 -0700

    new sharing data

--- ProIPhone_Ch07_SharingDataBetweenStructures/NewTopLevelView.swift
+++ ProIPhone_Ch07_SharingDataBetweenStructures/NewTopLevelView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct NewTopLevelView: View {
+    @State private var isDarkMode = false
+     @ObservedObject private var student = Student()
+     @ObservedObject private var course = Course()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+                   DarkModeToggleView(isDarkMode: $isDarkMode)
+
+                   StudentEditorView(student: student)
+                   Text("Student Information:")
+                   Text("First Name: \(student.firstName)")
+                   Text("Last Name: \(student.lastName)")
+
+                   Divider()
+
+                   CourseEditorView(course: course)
+                   Text("Course Information:")
+                   Text("Year: \(course.year)")
+                   Text("Quarter: \(course.quarter)")
+                   Text("Area: \(course.area)")
+                   Text("Course Number: \(course.courseNumber)")
+                   Text("Course Title: \(course.courseTitle)")
+               }
     }
 }
 
-#Preview {
-    NewTopLevelView()
+struct DarkModeToggleView: View {
+    @Binding var isDarkMode: Bool
+
+    var body: some View {
+        Toggle("Dark Mode", isOn: $isDarkMode)
+            .padding()
+    }
+}
+
+struct NewTopLevelView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTopLevelView()
+    }
 }
