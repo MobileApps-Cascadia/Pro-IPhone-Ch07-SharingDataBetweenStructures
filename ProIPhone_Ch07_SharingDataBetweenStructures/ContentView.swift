c8de050203084f958392e34bf285bc5bda8cc1cb
Kenny <kennypluchau@gmail.com>
2023-10-02 12:48:07 -0700

    Mac is dying

--- ProIPhone_Ch07_SharingDataBetweenStructures/ContentView.swift
+++ ProIPhone_Ch07_SharingDataBetweenStructures/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var Darkmode = false
+    @State private var student = Student(firstName: "Kenny", lastName: "Luchau")
+    @State private var course = Course( year: "2023", quarter: "Q1", field: "IT-MOB", courseNumber: "101", courseTitle: "Intro to swift")
+    
     var body: some View {
         TabView {
             RandomBackgroundColor()
@@ -24,16 +28,13 @@ struct ContentView: View {
                 .tabItem {
                     Image(systemName: "3.circle")
                 }
-            EnvironmentObjectView()
+            DarkmodeAndInfo(isDarkMode: $Darkmode, student: $student, course: $course)
                 .tabItem {
                     Image(systemName: "4.circle")
                 }
-            ch07()
-                .tabItem{
-                    Image(systemName: "5.circle")
-                }
         }
         .tabViewStyle(DefaultTabViewStyle())
+        .environment(\.colorScheme, Darkmode ? .dark : .light)
 
     }
 }
@@ -41,5 +42,66 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Student {
+    var firstName: String
+    var lastName: String
+}
+
+struct Course {
+    var year: String
+    var quarter: String
+    var field: String
+    var courseNumber: String
+    var courseTitle: String
+}
+
+struct DarkmodeAndInfo: View {
+    @Binding var isDarkMode: Bool
+    @Binding var student: Student
+    @Binding var course: Course
+
+    var body: some View {
+        VStack {
+            Toggle("Dark Mode", isOn: $isDarkMode)
+                .padding()
+
+            Text("Student Information:")
+                .font(.headline)
+
+            StudentInfoView(student: $student)
+
+            Text("Course Information:")
+                .font(.headline)
+            CourseInfoView(course: $course)
+        }
+        .padding()
+    }
+}
+
+struct StudentInfoView: View {
+    @Binding var student: Student
+
+    var body: some View {
+        VStack {
+            TextField("First Name", text: $student.firstName)
+            TextField("Last Name", text: $student.lastName)
+        }
+    }
+}
+
+struct CourseInfoView: View {
+    @Binding var course: Course
+
+    var body: some View {
+        VStack {
+            TextField("Year", text: $course.year)
+            TextField("Quarter", text: $course.quarter)
+            TextField("Area", text: $course.field)
+            TextField("Course Number", text: $course.courseNumber)
+            TextField("Course Title", text: $course.courseTitle)
+        }
     }
 }
